3210fca3d5edc9c8551b5acc5731fbccbc64b364
SwiftLiu <FineexMac@mac-mini.local>
2016-02-16 17:39:11 +0800

    丰富了Demo内容

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -22,13 +22,38 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (IBAction)creatDB:(id)sender {
-    NSString *path = [NSString stringWithFormat:@"%@/Documents/Test.sqlite", NSHomeDirectory()];
+- (IBAction)database:(id)sender {
+    //注意：在工程导入类似sqlite的资源文件时，需要create folder references 而不是create groups，否则此方法无法找到路径
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Test.sqlite" ofType:nil];
+    NSLog(@"%@", path);
+    
     sqlite = [LPSqlite sqlitePath:path];
+    
+    [self doDatabase];
 }
 
-- (IBAction)openDB:(id)sender {
-    [sqlite updateWithSql:nil];
+- (IBAction)localDatabase:(id)sender {
+    NSString *path = [NSString stringWithFormat:@"%@/Documents/Test2.sqlite", NSHomeDirectory()];
+    NSLog(@"%@", path);
+    
+    sqlite = [LPSqlite sqlitePath:path];
+    
+    [self doDatabase];
+}
+
+- (void)doDatabase
+{
+    //插入表
+    [sqlite updateWithSql:@"CREATE TABLE IF NOT EXISTS TestTable(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER, score FLOAT)"];
+    //插入行
+    for (int i=0; i<3; i++) {
+        NSString *sql = [NSString stringWithFormat:@"INSERT INTO TestTable (name, age, score) VALUES ('刘刘%d', %d, %d)", arc4random()%100, arc4random()%50+20, arc4random()%100];
+        [sqlite updateWithSql:sql];
+    }
+    //查询数据
+    NSArray *result = [sqlite queryFrom:@"TestTable" where:nil equal:nil];
+    NSLog(@"%@", result);
+    
 }
 
 
