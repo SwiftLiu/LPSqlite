9c92ccde54c344f8cc597b99f89158e9efd043dc
SwiftLiu <FineexMac@Mac-mini.local>
2016-02-16 16:11:01 +0800

    V2.0
    将类方法改为了实例方法

--- LPSqlite/LPSqlite.h
+++ LPSqlite/LPSqlite.h
@@ -1,9 +1,12 @@
 //
-//  LPDatabase.h
-//  FineExAPP
+//  LPSqlite.h
 //
-//  Created by FineexMac on 16/2/3.
-//  Copyright © 2016年 FineEX-LF. All rights reserved.
+//  Created by iOS_Liu on 16/2/3.
+//  Copyright © 2016年 iOS_Liu. All rights reserved.
+//
+//  下载链接 https://github.com/SwiftLiu/LPSqlite.git
+//  作者GitHub主页 https://github.com/SwiftLiu
+//  作者邮箱 1062014109@qq.com
 //
 
 #import <Foundation/Foundation.h>
@@ -11,20 +14,22 @@
 
 @interface LPSqlite : NSObject
 
+///便利初始化（该路径没有数据库则创建）
++ (LPSqlite *)sqlitePath:(NSString *)path;
 
-
-///查询 指定表 指定行 指定字段信息
-+ (NSArray *)queryDBName:(NSString *)dbName from:(NSString *)tableName where:(NSString *)columnName equal:(NSString *)target;
-///查询 指定表 指定行 指定字段信息
-+ (NSArray *)queryDBName:(NSString *)dbName select:(NSString *)queryObject from:(NSString *)tableName where:(NSString *)columnName equal:(NSString *)target;
-///查询 指定表 指定行 指定字段信息 附加
-+ (NSArray *)queryDBName:(NSString *)dbName select:(NSString *)queryObject from:(NSString *)tableName where:(NSString *)columnName equal:(NSString *)target other:(NSString *)other;
-
-///本地创建db文件
-+ (BOOL)createDBWithPath:(NSString *)path;
 ///查询
-+ (NSArray *)queryDBName:(NSString *)dbName sql:(NSString *)sql;
-///更新
-+ (NSInteger)updateDBName:(NSString *)dbName sql:(NSString *)sql;
+- (NSArray *)queryWithSql:(NSString *)sql;
+///更新（创建表，删除、插入、更改数据等）
+- (NSInteger)updateWithSql:(NSString *)sql;
+
+
+///查询 指定表 指定行 指定字段信息
+- (NSArray *)queryFrom:(NSString *)tableName where:(NSString *)columnName equal:(NSString *)target;
+///查询 指定表 指定行 指定字段信息
+- (NSArray *)querySelect:(NSString *)queryObject from:(NSString *)tableName where:(NSString *)columnName equal:(NSString *)target;
+///查询 指定表 指定行 指定字段信息 附加
+- (NSArray *)querySelect:(NSString *)queryObject from:(NSString *)tableName where:(NSString *)columnName equal:(NSString *)target other:(NSString *)other;
+
+
 
 @end
